17418b9a862919e143552afa98344fe08de06b4c
rebeca <rebecanoyait@gmail.com>
2023-12-04 22:45:39 +0100

    Feat: Carousel added
    Carousel (No finalizado)

---
+++ js/mainCarousel.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
